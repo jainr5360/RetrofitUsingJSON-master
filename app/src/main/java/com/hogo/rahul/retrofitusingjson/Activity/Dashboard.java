da880106590978c93a61ea29ca7695fa428328ad
Rahul jain <jainr5360@gmail.com>
2017-11-08 00:05:38 +0530

    Retrofit with post value

--- app/src/main/java/com/hogo/rahul/retrofitusingjson/Activity/Dashboard.java
+++ app/src/main/java/com/hogo/rahul/retrofitusingjson/Activity/Dashboard.java
@@ -1,10 +1,15 @@
 package com.hogo.rahul.retrofitusingjson.Activity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.gson.GsonBuilder;
@@ -34,8 +39,22 @@ public class Dashboard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
         rvMenu = (RecyclerView) findViewById(R.id.rv_menu);
-        // getData();
-        getDependentList(1);
+        getData();
+//        getDependentList(1);
+
+        rvMenu.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), rvMenu, new ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Intent intent = new Intent(getApplicationContext(), SubItemActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
+        }));
+
     }
 
     private void getData() {
@@ -49,21 +68,10 @@ public class Dashboard extends AppCompatActivity {
                 Toast.makeText(Dashboard.this, "sucess" + gsonModel, Toast.LENGTH_SHORT).show();
                 mlist = (ArrayList<Userdatum>) gsonModel.getUserdata();
                 try {
-//                    for (int i = 0; i < mlist.size(); i++) {
-//
-//                        Userdatum details = new Userdatum();
-//                        name = details.getName();
-//                        String id = details.getId();
-//                        details.setId(id);
-//                        details.setName(name);
-////                        details.setImg(image);
-//                        detailsList.add(details);
-//                        Log.d("Menu", "Menu details : " + id + name);
-//                    }
                     if (!mlist.isEmpty()) {
-//                        adapter = new MenuHomeAdapter(getApplicationContext(), mlist);
-//                        rvMenu.setAdapter(adapter);
-//                        rvMenu.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                        adapter = new MenuHomeAdapter(getApplicationContext(), mlist);
+                        rvMenu.setAdapter(adapter);
+                        rvMenu.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
                     } else {
                         Toast.makeText(getApplicationContext(), "List is empty", Toast.LENGTH_SHORT).show();
                     }
@@ -81,29 +89,85 @@ public class Dashboard extends AppCompatActivity {
         });
     }
 
-    private void getDependentList(int PrimaryID) {
+    //    private void getDependentList(int PrimaryID) {
+//
+//        retrofit2.Call<MyResponse> call = null;
+//        call = Utils.getWebService().getData(1);
+//        Log.e("115 ", ": :" + call.request().url().toString());
+//        call.enqueue(new Callback<MyResponse>() {
+//            @Override
+//            public void onResponse(retrofit2.Call<MyResponse> call, Response<MyResponse> response) {
+//                Log.e("DependentList", " : " + new GsonBuilder().create().toJson(response.body()));
+//                Log.e("DependentList", " : " + response.code());
+//
+//                MyResponse myResponse = response.body();
+//
+//                adapter = new MenuHomeAdapter(getApplicationContext(), myResponse.getUserdata());
+//                rvMenu.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+//                rvMenu.setAdapter(adapter);
+//
+//            }
+//
+//            @Override
+//            public void onFailure(retrofit2.Call<MyResponse> call, Throwable t) {
+//
+//            }
+//        });
+//    }
+    public static interface ClickListener {
 
-        retrofit2.Call<MyResponse> call = null;
-        call = Utils.getWebService().getData(1);
-        Log.e("115 ", ": :" + call.request().url().toString());
-        call.enqueue(new Callback<MyResponse>() {
-            @Override
-            public void onResponse(retrofit2.Call<MyResponse> call, Response<MyResponse> response) {
-                Log.e("DependentList", " : " + new GsonBuilder().create().toJson(response.body()));
-                Log.e("DependentList", " : " + response.code());
+        public void onClick(View view, int position);
 
-                MyResponse myResponse = response.body();
-
-                adapter = new MenuHomeAdapter(getApplicationContext(), myResponse.getUserdata());
-                rvMenu.setAdapter(adapter);
-                rvMenu.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-
-            }
-
-            @Override
-            public void onFailure(retrofit2.Call<MyResponse> call, Throwable t) {
-
-            }
-        });
+        public void onLongClick(View view, int position);
     }
+
+    private class RecyclerTouchListener implements RecyclerView.OnItemTouchListener {
+
+        GestureDetector detector;
+        Dashboard.ClickListener clickListener;
+
+        public RecyclerTouchListener(Context context, RecyclerView view, final ClickListener listener) {
+
+            this.clickListener = listener;
+            detector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+
+                @Override
+                public boolean onSingleTapUp(MotionEvent e) {
+                    return true;
+                }
+
+                @Override
+                public void onLongPress(MotionEvent e) {
+                    super.onLongPress(e);
+
+                    View child = rvMenu.findChildViewUnder(e.getX(), e.getY());
+                    if (child != null && clickListener != null) {
+
+                        clickListener.onLongClick(child, rvMenu.getChildAdapterPosition(child));
+                    }
+                }
+            });
+        }
+
+        @Override
+        public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+
+            View child = rv.findChildViewUnder(e.getX(), e.getY());
+            if (child != null && clickListener != null && detector.onTouchEvent(e)) {
+                clickListener.onClick(child, rv.getChildAdapterPosition(child));
+            }
+            return false;
+        }
+
+        @Override
+        public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+        }
+
+        @Override
+        public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+        }
+    }
+
 }
